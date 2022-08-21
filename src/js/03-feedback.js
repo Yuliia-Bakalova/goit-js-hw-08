19de8d3711372650a7579717dd7ed53915ce694f
Yuliia-Bakalova <bakalova-j@ukr.net>
2022-08-21 21:50:55 +0300

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,32 +1,33 @@
-import throttle from 'lodash.throttle';
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  email: document.querySelector('.feedback-form input'),
-  message: document.querySelector('.feedback-form textarea'),
-};
+import throttle from 'lodash.throttle'
 
-refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', throttle(onFormInput, 500));
-const formData = {};
-const STORAGE_KEY = 'feedback-form-state';
-populateFeedback();
+const form = document.querySelector('form');
+  
+   
 
-function onFormSubmit(evt) {
-  evt.preventDefault();
-  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
-  evt.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
+const STORAGE_KEY = "feedback-form-state";
+const formData ={};
+form.addEventListener('submit', onSubmitBtn);
+form.addEventListener('input', throttle(onTextInput, 1000));
+
+ populateText();
+
+function onSubmitBtn(e){
+    e.preventDefault(); //убираем перезагрузку страницы
+    console.log('Send');
+    e.currentTarget.reset(); // очищаем поля формы
+    localStorage.removeItem(STORAGE_KEY);
+
+}
+function onTextInput(e){
+    formData.email = form.elements.email.value;
+    formData.message = form.elements.message.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
 }
 
-function onFormInput(evt) {
-  formData[evt.target.name] = evt.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-}
-
-function populateFeedback() {
-  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  if (savedData) {
-    refs.email.value = savedData.email;
-    refs.message.value = savedData.message;
-  }
+function populateText(){
+   const savedText =localStorage.getItem(STORAGE_KEY);
+   if(savedText){
+    form.email.value = JSON.parse(savedText).email;    
+    form.message.value = JSON.parse(savedText).message;
+    } 
 }
